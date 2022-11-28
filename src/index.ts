49ef5fce3a871859d563d55f4cbbfb0d88ffa860
Bun <bunpatta@gmail.com>
2022-11-28 15:04:42 +0700

    add events handler

--- src/index.ts
+++ src/index.ts
@@ -52,28 +52,43 @@ for (const file of commandFiles) {
   }
 }
 
-client.on(Events.InteractionCreate, async (interaction: any) => {
-  if (!interaction.isChatInputCommand()) return;
-  const command = interaction.client.commands.get(interaction.commandName);
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".ts"));
 
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
   }
+}
 
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
-  }
-});
+// client.on(Events.InteractionCreate, async (interaction: any) => {
+//   if (!interaction.isChatInputCommand()) return;
+//   const command = interaction.client.commands.get(interaction.commandName);
 
-client.once(Events.ClientReady, (c: any) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+//   if (!command) {
+//     console.error(`No command matching ${interaction.commandName} was found.`);
+//     return;
+//   }
+
+//   try {
+//     await command.execute(interaction);
+//   } catch (error) {
+//     console.error(error);
+//     await interaction.reply({
+//       content: "There was an error while executing this command!",
+//       ephemeral: true,
+//     });
+//   }
+// });
+
+// client.once(Events.ClientReady, (c: any) => {
+//   console.log(`Ready! Logged in as ${c.user.tag}`);
+// });
 
 client.login(process.env.TOKEN);
